22eff80a81303dda63f001ed12afeee3634fb45b
Laëtitia <laporte.laetitia@outlook.fr>
2025-11-23 14:37:03 +0100

    Upgrades
    Les minerais aliens permettent de miner plus vite (10 alienores = +1 upgrade)

--- scripts/UIMine.gd
+++ scripts/UIMine.gd
@@ -6,15 +6,20 @@ func _ready() -> void:
 	update_text()
 	GM.connect("ores_changed", _on_ores_changed)
 	GM.connect("ammo_changed", _on_ammo_changed)
+	GM.connect("upgrades_changed", _on_upgrades_changed)
 	
 func update_text():
 	self.text = "Ressources : " + str(GM.ores) \
 		+ "\nJ1 - Missiles : " + str(GM.heavy_ammo[0]) + " / Lightballs : " + str(GM.light_ammo[0]) \
-		+ "\nJ2 - Missiles : " + str(GM.heavy_ammo[1]) + " / Lightsballs : " + str(GM.light_ammo[1])
+		+ "\nJ2 - Missiles : " + str(GM.heavy_ammo[1]) + " / Lightsballs : " + str(GM.light_ammo[1]) \
+		+ "\nAméliorations : Minage +" + str(GM.upgrades)
 
 
 func _on_ores_changed():
 	update_text()
 	
 func _on_ammo_changed():
+	update_text()
+	
+func _on_upgrades_changed():
 	update_text()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -4,13 +4,15 @@ var light_ammo: Array[int] = [100, 100]
 var heavy_ammo: Array[int] = [10, 10]
 
 var ores := 0
-var alienOres := 0
+var alienOres := 10
+var upgrades := 0
 
 signal ores_changed()
 signal ammo_changed()
+signal upgrades_changed()
 
 func add_ore():
-	ores += 1
+	ores += 1 + upgrades
 	emit_signal("ores_changed")
 
 func add_heavy_ammo(player: int, amount: int = 2):
@@ -31,3 +33,9 @@ func add_light_ammo(player: int, amount: int = 1):
 		emit_signal("ores_changed")
 	else:
 		print("Pas assez de minerais !")
+
+func _process(_delta: float) -> void:
+	if alienOres >= 10:
+		upgrades += 1
+		alienOres -= 10
+		emit_signal("upgrades_changed")
