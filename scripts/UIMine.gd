ddd4bf5cdc8cebfbd6e619904d9967de80d78e60
Rashnain <51191602+Rashnain@users.noreply.github.com>
2025-11-25 01:08:53 +0100

    always update mining text

--- scripts/UIMine.gd
+++ scripts/UIMine.gd
@@ -5,24 +5,10 @@ var stat_template: String
 
 func _ready() -> void:
 	stat_template = text
-	update_text()
-	GM.connect("ores_changed", _on_ores_changed)
-	GM.connect("ammo_changed", _on_ammo_changed)
-	GM.connect("upgrades_changed", _on_upgrades_changed)
-	
-func update_text():
+
+func _process(_delta: float) -> void:
 	var player_index := 1-GM.player_index_bunker
 	GM.mine_text = stat_template % [GM.ores[player_index], GM.alien_ores[player_index], GM.upgrades[player_index]]
-
-
-func _on_ores_changed():
-	update_text()
-	
-func _on_ammo_changed():
-	update_text()
-	
-func _on_upgrades_changed():
-	update_text()
 
 func _on_half_day_passed() -> void:
 	GM.player_index_bunker = (GM.player_index_bunker + 1) % 2
